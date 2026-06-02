9ed479b219803b3323f85e06511f49e021218074
Seeni <20085943+seeni-dev@users.noreply.github.com>
2026-06-02 10:12:50 +0530

    Add: meeting-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-4.py
@@ -0,0 +1,25 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+from collections import defaultdict
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        timeline = defaultdict(dict)
+        for interval in intervals:
+            start, end = interval.start, interval.end
+            timeline[start]["start"] = timeline[start].get("start", 0) + 1
+            timeline[end]["end"] = timeline[end].get("end",0) + 1
+        
+        timestamps = sorted(timeline.keys())
+        meetingRooms = 0
+        maxMeetingRooms = 0
+        for timestamp in timestamps:
+            meetingRooms += timeline[timestamp].get("start", 0)
+            meetingRooms -=  timeline[timestamp].get("end", 0)
+            maxMeetingRooms = max(maxMeetingRooms, meetingRooms)
+
+        return maxMeetingRooms
