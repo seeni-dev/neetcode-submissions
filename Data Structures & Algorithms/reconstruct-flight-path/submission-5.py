81a8d2735b12f4f5cfca053b76aff02e87d2f3e9
Seeni <20085943+seeni-dev@users.noreply.github.com>
2026-06-19 18:50:20 +0530

    Add: reconstruct-flight-path - submission-5

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-5.py
@@ -0,0 +1,32 @@
+# city -> [destinations]
+# 
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        ans = None
+        def visit(path, ticketsGraph,ticketsCount, currentCity):
+            nonlocal ans
+            path = path + [currentCity]
+            if ticketsCount == 0:
+                if ans is None:
+                    ans = path
+                return
+            tickets = ticketsGraph.get(currentCity, {})
+            cities = sorted(filter(lambda x: tickets.get(x, 0) > 0, tickets.keys()))
+            for city in cities:
+                ticketsGraph[currentCity][city] -=1
+                visit(path, ticketsGraph, ticketsCount - 1, city)
+                if ans is not None:
+                    return
+                ticketsGraph[currentCity][city] +=1
+        
+        graph = {}
+        for ticket in tickets:
+            source, dest = ticket 
+            if source not in graph:
+                graph[source] = {}
+            if dest not in graph[source]:
+                graph[source][dest] = 0
+            graph[source][dest] +=1
+        visit([], graph, len(tickets), "JFK")
+        return ans
