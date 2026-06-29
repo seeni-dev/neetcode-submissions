79750399079c19773764c8096b9cb6d57046a9c7
Seeni <20085943+seeni-dev@users.noreply.github.com>
2026-06-29 08:43:19 +0530

    Add: meeting-schedule - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule/submission-6.py
@@ -0,0 +1,18 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if not intervals:
+            return True
+        intervals = sorted(intervals , key = lambda x: [x.start, x.end])
+        lastInterval = intervals[0]
+        for interval in intervals[1:]:
+            if interval.start in range(lastInterval.start, lastInterval.end):
+                return False
+            lastInterval = interval
+        return True
